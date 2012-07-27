7f1920f737aeeaece11079909ca02a1d8b6230b5
johnciacia <john@voceconnect.com>
2012-07-26 20:40:15 -0400

    Initial Commit

---
+++ helios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helios
+//
+//  Created by John Ciacia on 7/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ helios/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  helios
+//
+//  Created by John Ciacia on 7/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
